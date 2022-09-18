75a7f75dbbe79c1ad3574d34dc2284715c975434
leo <leoluo0115@qq.com>
2022-09-17 21:43:36 -0500

    Update models.py

--- apps/user/models.py
+++ apps/user/models.py
@@ -11,9 +11,11 @@ PAYMENT_METHOD_CHOICES = (
 
 
 class Account(models.Model):
-    name = models.CharField(max_length=30)
+    name = models.CharField(max_length=30)  # 字段长度
     mailing_address = models.CharField(max_length=200)
     billing_address = models.CharField(max_length=200)
-    star_points = models.FloatField(max_length=200, default=0)
+    star_points = models.FloatField(max_length=200, default=0)  # Float类型字段要加 default
+    #  首选支付方式使用整数类型来映射到全局变量 PAYMENT_METHOD_CHOICES, 如果什么都没穿需要加上 null=True, blank=True
     preferred_payment_method = models.IntegerField(choices=PAYMENT_METHOD_CHOICES, null=True, blank=True)
+    #  user来连接到 Django 自带的 User list / on_delete 当User删除，account也会被删除因为是一对一的关系
     user = models.OneToOneField(User, on_delete=models.CASCADE)
